143d1df7af72b4cd81abb600fd32763fcf92243f
toanpham2111 <toan444666@gmail.com>
2024-12-20 10:58:23 +0700

    update plugin

---
+++ src/controller/zippy-booking-controller.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+ * API ResponHandler
+ *
+ * @package Shin
+ */
+
+namespace Zippy_Booking\Src\Controller;
+
+use WP_REST_Request;
+use Zippy_Booking\Utils\Zippy_Response_Handler; 
+
+defined('ABSPATH') or die();
+
+class Zippy_Booking_Controller
+{
+    public static function create_booking_with_product(WP_REST_Request $request)
+    {
+        global $wpdb;
+        $table_name = 'fcs_data_bookings';
+
+        $product_id = intval($request->get_param('product_id'));
+        $user_id = intval($request->get_param('user_id'));
+        $email = sanitize_email($request->get_param('email'));
+        $booking_start_date = sanitize_text_field($request->get_param('booking_start_date'));
+        $booking_end_date = sanitize_text_field($request->get_param('booking_end_date'));
+
+        if (empty($product_id) || empty($email) || empty($booking_start_date) || empty($booking_end_date)) {
+            return Zippy_Response_Handler::error('Missing request parameter.');
+        }
+
+        $product = wc_get_product($product_id);
+        if (!$product) {
+            return Zippy_Response_Handler::error('Product does not exist.');
+        }
+
+        if (empty($user_id)) {
+            $user = get_user_by('email', $email);
+
+            if (!$user) {
+                $user_data = array(
+                    'user_login' => $email,
+                    'user_email' => $email,
+                    'user_pass' => wp_generate_password(),
+                    'role' => 'customer',
+                );
+
+                $user_id = wp_insert_user($user_data);
+
+                if (is_wp_error($user_id)) {
+                    return Zippy_Response_Handler::error('User creation failed.');
+                }
+            } else {
+                $user_id = $user->ID;
+            }
+        }
+
+        $wpdb->insert($table_name, array(
+            'user_id' => $user_id,
+            'email' => $email,
+            'product_id' => $product_id,
+            'booking_start_date' => $booking_start_date,
+            'booking_end_date' => $booking_end_date,
+            'booking_status' => 'pending',
+        ));
+
+        return Zippy_Response_Handler::success(
+            array(
+                'user_id' => $user_id,
+                'email' => $email,
+                'product_id' => $product_id,
+                'booking_start_date' => $booking_start_date,
+                'booking_end_date' => $booking_end_date,
+                'booking_status' => 'pending'
+            ),
+            'Booking created successfully.'
+        );
+    }
+    public static function get_booking_with_product(WP_REST_Request $request)
+    {
+        global $wpdb;
+        $table_name = 'fcs_data_bookings';
+
+        $booking_id = $request->get_param('booking_id');
+        $email = $request->get_param('email');
+        $product_id = $request->get_param('product_id');
+
+        $query = "SELECT * FROM $table_name WHERE 1=1";
+
+        if ($booking_id) {
+            $query .= $wpdb->prepare(" AND ID = %d", $booking_id);
+        }
+        if ($email) {
+            $query .= $wpdb->prepare(" AND email = %s", $email);
+        }
+        if ($product_id) {
+            $query .= $wpdb->prepare(" AND product_id = %d", $product_id);
+        }
+
+        $results = $wpdb->get_results($query);
+
+        if (empty($results)) {
+            return Zippy_Response_Handler::error('Booking not found.');
+        }
+
+        return Zippy_Response_Handler::success(
+            !empty($results) ? $results[0] : array(),
+            'Bookings retrieved successfully.'
+        );
+    }
+}
--- src/routers/zippy-bookings-router.php
+++ src/routers/zippy-bookings-router.php
@@ -8,12 +8,10 @@
 
 namespace Zippy_Booking\Src\Routers;
 
+use Zippy_Booking\Src\Controller\Zippy_Booking_Controller;
 
 defined('ABSPATH') or die();
 
-use WP_REST_Request;
-use WP_Error;
-use Zippy_Booking\Utils\Zippy_Response_Handler;
 
 class Zippy_Bookings_Router
 {
@@ -39,9 +37,9 @@ class Zippy_Bookings_Router
 
     public function zippy_booking_init_api()
     {
-        register_rest_route('zippy-booking/v1', '/booking', array(
+        register_rest_route(ZIPPY_BOOKING_API_NAMESPACE, '/booking', array(
             'methods' => 'GET',
-            'callback' =>  array($this, 'get_booking_with_product'),
+            'callback' => array(new Zippy_Booking_Controller(), 'get_booking_with_product'),
             'args' => array(
                 'booking_id' => array(
                     'required' => false,
@@ -65,9 +63,9 @@ class Zippy_Bookings_Router
             'permission_callback' => '__return_true',
         ));
 
-        register_rest_route('zippy-booking/v1', '/booking', array(
+        register_rest_route(ZIPPY_BOOKING_API_NAMESPACE, '/booking', array(
             'methods' => 'POST',
-            'callback' => array($this, 'create_booking_with_product'),
+            'callback' => array(new Zippy_Booking_Controller(), 'create_booking_with_product'),
             'args' => array(
                 'product_id' => array(
                     'required' => true,
@@ -102,102 +100,5 @@ class Zippy_Bookings_Router
             ),
             'permission_callback' => '__return_true',
         ));
-    }
-
-    public function create_booking_with_product(WP_REST_Request $request)
-    {
-        global $wpdb;
-        $table_name = 'fcs_data_bookings';
-
-        $product_id = intval($request->get_param('product_id'));
-        $user_id = intval($request->get_param('user_id'));
-        $email = sanitize_email($request->get_param('email'));
-        $booking_start_date = sanitize_text_field($request->get_param('booking_start_date'));
-        $booking_end_date = sanitize_text_field($request->get_param('booking_end_date'));
-
-        if (empty($product_id) || empty($email) || empty($booking_start_date) || empty($booking_end_date)) {
-            return new WP_Error('missing_parameters', 'Missing request parameter.', array('status' => 'error'));
-        }
-
-        $product = wc_get_product($product_id);
-        if (!$product) {
-            return new WP_Error('invalid_product', 'Product does not exist.', array('status' => 'error'));
-        }
-
-        if (empty($user_id)) {
-            $user = get_user_by('email', $email);
-
-            if (!$user) {
-                $user_data = array(
-                    'user_login' => $email,
-                    'user_email' => $email,
-                    'user_pass' => wp_generate_password(),
-                    'role' => 'customer',
-                );
-
-                $user_id = wp_insert_user($user_data);
-
-                if (is_wp_error($user_id)) {
-                    return new WP_Error('user_creation_failed', 'User creation failed.', array('status' => 'error'));
-                }
-            } else {
-                $user_id = $user->ID;
-            }
-        }
-
-        $wpdb->insert($table_name, array(
-            'user_id' => $user_id,
-            'email' => $email,
-            'product_id' => $product_id,
-            'booking_start_date' => $booking_start_date,
-            'booking_end_date' => $booking_end_date,
-            'booking_status' => 'pending',
-        ));
-
-        return Zippy_Response_Handler::success(
-            array(
-                'user_id' => $user_id,
-                'email' => $email,
-                'product_id' => $product_id,
-                'booking_start_date' => $booking_start_date,
-                'booking_end_date' => $booking_end_date,
-                'booking_status' => 'pending'
-            ),
-            'Booking created successfully.'
-        );
-    }
-    public function get_booking_with_product(WP_REST_Request $request)
-    {
-        global $wpdb;
-        $table_name = 'fcs_data_bookings';
-
-        $booking_id = $request->get_param('booking_id');
-        $email = $request->get_param('email');
-        $product_id = $request->get_param('product_id');
-
-        $query = "SELECT * FROM $table_name WHERE 1=1";
-
-        if ($booking_id) {
-            $query .= $wpdb->prepare(" AND ID = %d", $booking_id);
-        }
-        if ($email) {
-            $query .= $wpdb->prepare(" AND email = %s", $email);
-        }
-        if ($product_id) {
-            $query .= $wpdb->prepare(" AND product_id = %d", $product_id);
-        }
-
-        $results = $wpdb->get_results($query);
-
-        if (empty($results)) {
-            return new WP_Error('no_booking', 'Booking not found.', array('status' => 404));
-        }
-
-        return Zippy_Response_Handler::success(
-            array(
-                $results,
-            ),
-            'Bookings retrieved successfully.'
-        );
     }
 }
