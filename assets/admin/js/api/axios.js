c3704844aaebd0f04a2e98f74c9eb4b61433110b
shin <nhathanh024@gmail.com>
2024-12-30 16:17:29 +0700

    Update API

--- assets/admin/js/api/axios.js
+++ assets/admin/js/api/axios.js
@@ -1,12 +1,16 @@
 import { ErrorOutline } from "@mui/icons-material";
 import axios from "axios";
 
-export const makeRequest = async (endpoint, params = {}, method = "", token = "") => {
+export const makeRequest = async (
+  endpoint,
+  params = {},
+  method = "",
+  token = "NlLzbAaslg8FtjEArjkX0jo6OS9seUc3TGRKUmhJVytacjRHM1h2dz09"
+) => {
   const baseURL = "/wp-json";
   const api = axios.create({
     baseURL: baseURL,
   });
-
 
   const headers = token ? { Authorization: `Bearer ${token}` } : {};
 
@@ -14,22 +18,22 @@ export const makeRequest = async (endpoint, params = {}, method = "", token = ""
     url: "zippy-booking/v1" + endpoint,
     params: params,
     method: method,
-    headers: headers, 
+    headers: headers,
   };
 
   try {
     const res = await api.request(config);
     const data = res.data;
     // console.log(error);
-    
+
     return { data };
   } catch (error) {
     if (!error?.response) {
       console.error("❗Error", error.message);
-      return { error: error.message };  
+      return { error: error.message };
     }
 
     console.error("API Error:", error.response.statusText);
-    return { error: error.response.statusText };  
+    return { error: error.response.statusText };
   }
 };
